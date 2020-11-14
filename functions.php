983679664987e4865ccba714d3827b663c4e6537
Jeremy Felt <jeremy.felt@gmail.com>
2020-11-13 16:39:39 -0800

    Add initial theme files; some basic decisions

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Load general utilities available in the theme.
+ */
+require_once __DIR__ . '/includes/utilities.php';
+
+/**
+ * Load general theme setup hooks.
+ */
+require_once __DIR__ . '/includes/theme-setup.php';
---
+++ includes/utilities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Parcel\Utilities;
+
+/**
+ * Retrieve the theme's version number.
+ *
+ * @return string Current theme version number.
+ */
+function get_version() {
+	$version = wp_get_theme()->version;
+
+	return $version;
+}
